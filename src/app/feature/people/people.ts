ad34d80efb5b5cbefb2e44caffd30088f665ecff
Luis Morales <icluis.morales@americanwell.com>
2025-06-09 11:27:41 -0600

    Full features.

--- src/app/feature/people/people.ts
+++ src/app/feature/people/people.ts
@@ -1,35 +1,56 @@
 import { Component, inject, OnInit } from '@angular/core';
 
-import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Observable,
+} from 'rxjs';
 import { PeopleModel } from '../../shared/models/people.model';
 import { CommonModule } from '@angular/common';
 import { PeopleService } from '../../core/services/people.service';
 import { SortDirection, SortKey } from '../../shared/models/sort.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-people',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './people.html',
   styleUrl: './people.scss',
 })
 export class People implements OnInit {
   searchInput$ = new BehaviorSubject<string>('');
   sortColumn$ = new BehaviorSubject<keyof PeopleModel>('firstName');
-
   multiSort$ = new BehaviorSubject<SortKey[]>([]);
 
+  titleFilter$ = new BehaviorSubject<string>('all');
+  titleList$!: Observable<string[]>;
+
+  debounceSearch$!: Observable<string>;
   filteredPeople$!: Observable<PeopleModel[]>;
+
+  editFieldById: number = -1;
+  checkedPeopleList = new Set();
+
+  currentPage$ = new BehaviorSubject(1);
+  pageSize = 10;
 
   private peopleService = inject(PeopleService);
 
   ngOnInit(): void {
+    this.debounceSearch$ = this.searchInput$.pipe(debounceTime(300), distinctUntilChanged());
+
     this.filteredPeople$ = combineLatest([
       this.peopleService.getPeople(),
-      this.searchInput$,
+      this.debounceSearch$,
       this.multiSort$,
+      this.titleFilter$,
+      this.currentPage$,
     ]).pipe(
-      map(([people, search, multiSort]) => {
-        const filtered = people.filter((person) => {
+      map(([people, search, multiSort, titleFilter, currentPage]) => {
+        let filtered = people.filter((person) => {
           const searchable = [
             person.firstName,
             person.lastName,
@@ -43,6 +64,11 @@ export class People implements OnInit {
           return searchable.includes(search.toLowerCase());
         });
 
+        // Filters
+        filtered =
+          titleFilter !== 'all' ? filtered.filter((f) => f.title === titleFilter) : filtered;
+
+        // Sorting
         filtered.sort((a, b) => {
           for (const sort of multiSort) {
             const aVal = String(a[sort.column]).toLowerCase();
@@ -54,9 +80,15 @@ export class People implements OnInit {
           return 0;
         });
 
-        return filtered;
+        // Pagination
+        const start = (currentPage - 1) * this.pageSize;
+        const paginated = filtered.slice(start, start + this.pageSize);
+
+        return paginated;
       }),
     );
+
+    this.titleList$ = this.peopleService.getPeopleTitles();
   }
 
   handleOnSearchChange(value: string) {
@@ -78,5 +110,57 @@ export class People implements OnInit {
     ];
 
     this.multiSort$.next(newSort);
+  }
+
+  handleOnSortIcon(column: keyof PeopleModel): string {
+    const current = this.multiSort$.value;
+    const existing = current.find((s) => s.column === column);
+    let icon = '';
+
+    if (existing) {
+      icon = existing?.direction === 'asc' ? '↑' : '↓';
+    }
+
+    return icon;
+  }
+
+  handleOnEdit(id: number) {
+    this.editFieldById = id;
+  }
+
+  handleOnEditCancel() {
+    this.editFieldById = -1;
+  }
+
+  handleOnTitleFilter(title: string) {
+    this.titleFilter$.next(title);
+  }
+
+  handleChecked(id: number): void {
+    this.checkedPeopleList.has(id)
+      ? this.checkedPeopleList.delete(id)
+      : this.checkedPeopleList.add(id);
+  }
+
+  handleAllChecked(peopleList: PeopleModel[], checked: boolean): void {
+    checked
+      ? peopleList.forEach((p) => this.checkedPeopleList.add(p.id))
+      : peopleList.forEach((p) => this.checkedPeopleList.delete(p.id));
+  }
+
+  handleAreAllChecked(peopleList: PeopleModel[]): boolean {
+    return peopleList.every((p) => this.checkedPeopleList.has(p.id));
+  }
+
+  handleNextPage() {
+    const current = this.currentPage$.value;
+    this.currentPage$.next(current + 1);
+  }
+
+  handlePrevPage() {
+    const current = this.currentPage$.value;
+    if (current > 1) {
+      this.currentPage$.next(current - 1);
+    }
   }
 }
