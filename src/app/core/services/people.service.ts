e2932d7e05c82d40465b6b5c6115af2f75c388d8
Luis Morales <icluis.morales@americanwell.com>
2025-06-09 08:59:19 -0600

    Added mock data.

--- src/app/core/services/people.service.ts
+++ src/app/core/services/people.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PeopleModel } from '../../shared/models/people.model';
-import { Observable, of } from 'rxjs';
+import { delay, Observable, of } from 'rxjs';
+import { peopleList } from './people.mock';
 
 @Injectable({
   providedIn: 'root',
@@ -8,42 +9,9 @@ import { Observable, of } from 'rxjs';
 export class PeopleService {
   constructor() {}
 
-  private people: PeopleModel[] = [
-    {
-      id: 1,
-      firstName: 'John',
-      lastName: 'Doe',
-      dateOfBirth: '1985-05-15',
-      title: 'Engineer',
-      phoneNumber: '123-456-7890',
-    },
-    {
-      id: 2,
-      firstName: 'Jane',
-      lastName: 'Smith',
-      dateOfBirth: '1990-10-20',
-      title: 'Manager',
-      phoneNumber: '987-654-3210',
-    },
-    {
-      id: 3,
-      firstName: 'Alice',
-      lastName: 'Johnson',
-      dateOfBirth: '1975-03-22',
-      title: 'Designer',
-      phoneNumber: '555-123-4567',
-    },
-    {
-      id: 4,
-      firstName: 'Bob',
-      lastName: 'Williams',
-      dateOfBirth: '2000-12-01',
-      title: 'Developer',
-      phoneNumber: '444-333-2222',
-    },
-  ];
+  people = peopleList;
 
   getPeople(): Observable<PeopleModel[]> {
-    return of(this.people);
+    return of(this.people).pipe(delay(100));
   }
 }
